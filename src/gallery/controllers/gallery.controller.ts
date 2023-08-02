64dcbb1bce6b8b5f2f7b5c1b6a6c07334d88c5e0
DESKTOP-MJ9VO5P\gasto <gaston.caldera.developer@gmail.com>
2023-08-02 10:27:35 -0300

    add new route

--- src/gallery/controllers/gallery.controller.ts
+++ src/gallery/controllers/gallery.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 import { GalleryService } from '../services/gallery.service';
 
 @Controller('gallery')
 export class GalleryController {
   constructor(private galleryService: GalleryService) {}
   @Get()
-  getGallery() {
-    return this.galleryService.getGallery();
+  getGallery(
+    @Query('section') section: string,
+    @Query('sort') sort: string,
+    @Query('window') window: string,
+    @Query('page') page: number,
+  ) {
+    return this.galleryService.getGallery(section, sort, window, page);
+  }
+  @Get('post/:id')
+  getPostById(@Param('id') id: string) {
+    return this.galleryService.getPostById(id);
   }
 }
--- src/gallery/services/gallery.service.ts
+++ src/gallery/services/gallery.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GalleryService {
-  async getGallery() {
+  async getGallery(
+    section: string,
+    sort: string,
+    window: string,
+    page: number,
+  ) {
     try {
       const myHeaders = new Headers();
       const clientId = `Client-ID ${process.env.IMGUR_CLIENT_ID}`;
@@ -13,7 +18,26 @@ export class GalleryService {
         headers: myHeaders,
         redirect: 'follow',
       };
-      const apiUrl = `https://api.imgur.com/3/gallery/hot/viral/day/1}`;
+      const apiUrl = `https://api.imgur.com/3/gallery/${section}/${sort}/${window}/${page}}`;
+      const response = await fetch(apiUrl, requestOptions);
+      return response.json();
+    } catch (error) {
+      console.log('error', error);
+      return error;
+    }
+  }
+  async getPostById(id: string) {
+    try {
+      const myHeaders = new Headers();
+      const clientId = `Client-ID ${process.env.IMGUR_CLIENT_ID}`;
+      myHeaders.append('Authorization', clientId);
+
+      const requestOptions: RequestInit = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow',
+      };
+      const apiUrl = `https://api.imgur.com/3/gallery/album/${id}`;
       const response = await fetch(apiUrl, requestOptions);
       return response.json();
     } catch (error) {
